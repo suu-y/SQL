53a0fdf5c0f274778eb4ad7e46ab92dc1a07e3de
suu-y <cgug1090@mail4.doshisha.ac.jp>
2023-12-03 00:12:56 +0900

    解けていなかったImmediate_Food...を解いた

--- SQL50/M_Immediate-Food-Delivery-2.sql
+++ SQL50/M_Immediate-Food-Delivery-2.sql
@@ -1,20 +1,26 @@
 # Write your MySQL query statement below
-WITH isImmediate_order AS(
+WITH min_order_date AS (
     SELECT
         customer_id,
-        MIN(order_date) AS order_date,
-        CASE WHEN (order_date = MIN(customer_pref_delivery_date)) THEN 'immediate'
-            ELSE 'scheduled_order' END AS first_deliv_date
+        MIN(order_date) AS min_order_date
     FROM
         Delivery
     GROUP BY
         customer_id
+), order_type AS(
+    SELECT
+        CASE WHEN(delivery_id IS NULL) THEN 'imm'
+            ELSE 'sch' END AS type
+    FROM
+        min_order_date
+        LEFT OUTER JOIN Delivery
+        ON (
+            min_order_date.min_order_date = Delivery.customer_pref_delivery_date
+            AND
+            min_order_date.customer_id = Delivery.customer_id
+        )
 )
 SELECT
-    ROUND(SUM(CASE WHEN isImmediate_order.first_deliv_date = 'immediate' THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS immediate_percentage
+    ROUND(SUM(CASE WHEN type = 'imm' THEN 0 ELSE 1 END) * 100 / COUNT(*), 2) AS immediate_percentage
 FROM
-    Delivery
-    INNER JOIN isImmediate_order
-        ON Delivery.order_date = isImmediate_order.order_date
-
--- これは動くけど、求めている結果は得られていない
+    order_type
