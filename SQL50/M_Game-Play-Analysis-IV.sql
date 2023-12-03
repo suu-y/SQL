e96e03aa5c6a6fb05290594852d2e6b556e39004
suu-y <cgug1090@mail4.doshisha.ac.jp>
2023-12-03 22:38:14 +0900

    解けていなかったGame Play...を解いて、50問終了

--- SQL50/M_Game-Play-Analysis-IV.sql
+++ SQL50/M_Game-Play-Analysis-IV.sql
@@ -1,30 +1,14 @@
--- まだ解けていない
 # Write your MySQL query statement below
-WITH user_freq AS (
-    SELECT DISTINCT
-        a1.player_id AS pid,
-        MIN(a1.event_date) AS first_login
+WITH freq_user AS(
+    SELECT
+        player_id,
+        -- 真偽値を返す
+        DATEDIFF(event_date, MIN(event_date) OVER(PARTITION BY player_id)) = 1
+            AS consecutive
     FROM
-        Activity a1
-        INNER JOIN Activity a2
-            ON a1.player_id = a2.player_id
-    WHERE
-        DATEDIFF(a2.event_date, a1.event_date) = 1
-    GROUP BY
-        a1.player_id
+        Activity
 )
 SELECT
-    *,
-    ROUND(
-    COUNT(DISTINCT CASE WHEN first_login IS NOT NULL THEN a3.player_id END) /
-    COUNT(DISTINCT a3.player_id), 2
-    ) AS fraction
+    ROUND(SUM(consecutive) / COUNT(DISTINCT player_id), 2) AS fraction
 FROM
-    Activity a3
-    LEFT OUTER JOIN
-        user_freq
-        ON (
-            a3.player_id = user_freq.pid
-            AND
-            a3.event_date = user_freq.first_login
-        )
+    freq_user
