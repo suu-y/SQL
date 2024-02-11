27f10b4f9b5476a699d4590e98b8843fafc5637a
suu-y <cgug1090@mail4.doshisha.ac.jp>
2024-02-11 21:13:16 +0900

    Easyを5問解いた

---
+++ Problem_List/E_Customers-Who-Never_Order.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    Customers.name AS 'Customers'
+FROM
+    Customers
+    LEFT OUTER JOIN Orders
+        ON Customers.id = Orders.customerId
+WHERE
+    Orders.id IS NULL
